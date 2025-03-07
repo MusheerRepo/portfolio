b756c5f81cd97a9404413a93ed6a647a08842d77
Musheer <musheerakhan121@gmail.com>
2025-03-07 17:32:53 +0530

    AccessModifier

---
+++ AccessModifier.ts
@@ -0,0 +1,19 @@
+// Public is default access modifier
+// Private -> In the same class where it is declared
+// Protected -> Accessable in same class and also sub classes
+// Read-only modifier -> This can be initalized in the run time but can only be initialized once
+
+class Musheer {
+    readonly var12
+    
+    constructor() {
+        this.var12 = 12
+    }
+
+    print1() {
+        console.log(this.var12)
+    }
+}
+
+let x = new Musheer();
+x.print1()
