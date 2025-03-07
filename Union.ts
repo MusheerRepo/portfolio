967d19d9d678ba065505514c949471e9fc785865
Musheer <musheerakhan121@gmail.com>
2025-03-07 16:33:42 +0530

    Maps

--- Union.ts
+++ Union.ts
@@ -9,9 +9,9 @@ type x = 1|2|3|4|5|6|7|8|9|10|11|"Musheer"
 
 let t:x = "Musheer"
 
-var a:(string | number)[] = [1, 'Musheer']
-console.log(a[1])
+var a1A:(string | number)[] = [1, 'Musheer']
+console.log(a1A[1])
 
 //Tuple
-let b: [string, number] = ['Hello', 1]
+let b1A: [string, number] = ['Hello', 1]
 console.log(b)
