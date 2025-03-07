a505efd178f4cd8f311bad652542b28e41816dae
Musheer <musheerakhan121@gmail.com>
2025-03-07 13:39:42 +0530

    Started maps

--- Loops.ts
+++ Loops.ts
@@ -100,3 +100,9 @@ for (let i = 1; i <= 10; i++) {
     }
     console.log("Odd number:", i);
 }
+
+
+let var1 = [1,2,3,4,5,6,7,8,9,0]
+var1.forEach((v) => {
+    console.log(v % 2 === 0)
+})
--- Switch.ts
+++ Switch.ts
@@ -1,8 +1,19 @@
+let value = 1;
+
 switch(value) {
     case 1:
         console.log('One')
+        console.log("Another message")
         break
     default:
         console.log('Other numbers')
 }
 
+switch(value.toString()) {
+    case "1": 
+        console.log("One")
+        console.log("Another message")
+        break
+    default:
+        console.log("Another message")
+}
