c8686186ffb62d4b919218d5399e67b49934381b
Musheer <musheerakhan121@gmail.com>
2025-03-10 19:25:42 +0530

    AccessModifier

--- generators.ts
+++ generators.ts
@@ -10,3 +10,19 @@ while(!check11.done) {
     console.log(check11.value)
     check11 = x.next()
 }
+
+// Practice
+
+function* g1() {
+    yield [1,2]
+    yield 22
+    yield 'musheer'
+}
+
+let funcvar1 = g1()
+let check2 = funcvar1.next()
+
+while(!check2.done) {
+    console.log(check2.value)
+    check2 = funcvar1.next()
+}
